d1e2e771194c0f444181341cb1c71e06c74eb543
Cami9911 <48393552+Cami9911@users.noreply.github.com>
2020-03-19 14:44:56 +0200

    compulsory-laborator4

---
+++ lab4/Resident.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Resident {
+    private String name;
+    public Resident(String s) {
+        this.name=s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
