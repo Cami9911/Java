eaf192b65d071c035063c91d27b7bb71148e6ac2
Tîrnovan Camelia <48393552+Cami9911@users.noreply.github.com>
2020-04-09 15:43:56 +0300

    compulsory 8

---
+++ lab8/ro/uaic/info/myapp/dao/ArtistController.java
@@ -0,0 +1,11 @@
+package ro.uaic.info.myapp.dao;
+
+public class ArtistController {
+    public String create(String name, String country){
+     return
+    }
+
+    public String findByName(String name){
+        return
+    }
+}
